ca7f11a8287f82813303694cc112a83de4554270
PhilanthropiCat <philanthropicat@outlook.com>
2023-08-10 19:29:38 -0600

    Version 0.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = "0.1.3"
+VERSION = "0.1.6"
 DESCRIPTION = "PiCa is lightweight and minimalist package for autodiff."
 
 
